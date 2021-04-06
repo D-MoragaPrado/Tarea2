894d9da5ecb93e2c1df41dfc9ee6279a3440a916
D-MoragaPrado <diana.moragaprado@gmail.com>
2021-04-06 19:05:15 -0400

    ultimo

--- js/inicio.js
+++ js/inicio.js
@@ -1,11 +1,31 @@
 "use strict";
 var formulario = document.getElementById("formulario");
 var mensaje = document.getElementById("mensaje");
+var advertencia = document.getElementById("advertencia");
+var check1 = document.getElementById("check1");
+var check2 = document.getElementById("check2");
+var check3 = document.getElementById("check3");
+var check4 = document.getElementById("check4");
+var check5 = document.getElementById("check5");
+var check6 = document.getElementById("check6");
 function limpiarDatos() {
     formulario.reset();
 }
+function revisarCheckBox() {
+    if (check1.checked == true || check2.checked == true || check3.checked == true || check4.checked == true || check5.checked == true || check6.checked == true) {
+        return true;
+    }
+    else {
+        advertencia.style.display = "block";
+        check1.focus();
+        return false;
+    }
+}
 formulario.addEventListener("submit", function (evento) {
-    formulario.style.display = "none";
-    mensaje.style.display = "block";
-    evento.preventDefault();
+    if (revisarCheckBox()) {
+        formulario.style.display = "none";
+        mensaje.style.display = "block";
+        revisarCheckBox();
+        evento.preventDefault();
+    }
 });
--- ts/inicio.ts
+++ ts/inicio.ts
@@ -1,17 +1,32 @@
 let formulario:any=document.getElementById("formulario");
 let mensaje:any=document.getElementById("mensaje");
+let advertencia:any=document.getElementById("advertencia");
+let check1:any=document.getElementById("check1");
+let check2:any=document.getElementById("check2");
+let check3:any=document.getElementById("check3");
+let check4:any=document.getElementById("check4");
+let check5:any=document.getElementById("check5");
+let check6:any=document.getElementById("check6");
 
 
 function limpiarDatos(){
     formulario.reset();
 }
-
+function revisarCheckBox(){
+    if (check1.checked == true || check2.checked == true || check3.checked == true || check4.checked == true || check5.checked == true || check6.checked == true){
+        return true
+    }
+    else{
+        advertencia.style.display="block"
+        check1.focus();
+        return false
+    }
+}
 formulario.addEventListener("submit",function(evento:any){
-    formulario.style.display="none";
-    mensaje.style.display="block";
-    evento.preventDefault();
-
- });
-
-
-
+    if(revisarCheckBox()){
+        formulario.style.display="none";
+        mensaje.style.display="block";
+        revisarCheckBox();
+        evento.preventDefault();
+    }
+});
